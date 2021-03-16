531b2f5e927e6afbaef65514046e4ff3f9165835
amanusk <amanusk@kzencorp.com>
2021-03-16 10:56:47 +0200

    Add doge and dogetest networks

--- src/networks.js
+++ src/networks.js
@@ -7,21 +7,41 @@ module.exports = {
     bech32: 'bc',
     bip32: {
       public: 0x0488b21e,
-      private: 0x0488ade4
+      private: 0x0488ade4,
     },
     pubKeyHash: 0x00,
     scriptHash: 0x05,
-    wif: 0x80
+    wif: 0x80,
   },
   testnet: {
     messagePrefix: '\x18Bitcoin Signed Message:\n',
     bech32: 'tb',
     bip32: {
       public: 0x043587cf,
-      private: 0x04358394
+      private: 0x04358394,
     },
     pubKeyHash: 0x6f,
     scriptHash: 0xc4,
-    wif: 0xef
-  }
-}
+    wif: 0xef,
+  },
+  dogecoin: {
+    messagePrefix: '\x19Dogecoin Signed Message:\n',
+    bip32: {
+      public: 0x02facafd,
+      private: 0x02fac398,
+    },
+    pubKeyHash: 0x1e,
+    scriptHash: 0x16,
+    wif: 0x9e,
+  },
+  dogetest: {
+    messagePrefix: '\x19Dogecoin Signed Message:\n',
+    bip32: {
+      public: 0x0432a9a8,
+      private: 0x0432a243,
+    },
+    pubKeyHash: 0x71,
+    scriptHash: 0xc4,
+    wif: 0xf1,
+  },
+};
